e5d40cf97411bae4843e25e5ae20d956dd04bae9
Bernhard Boser <bernhard.e.boser@gmail.com>
2021-12-10 17:35:46 -0800

    rust, plotserver

---
+++ docs/projects/plotserver/bkapp/plotserver/bkapp.py
@@ -0,0 +1,29 @@
+from bokeh.layouts import column
+from bokeh.models import ColumnDataSource, Slider
+from bokeh.plotting import figure
+from bokeh.sampledata.sea_surface_temperature import sea_surface_temperature
+from bokeh.themes import Theme
+
+
+def bkapp(doc):
+    df = sea_surface_temperature.copy()
+    source = ColumnDataSource(data=df)
+
+    plot = figure(x_axis_type='datetime', y_range=(0, 25), y_axis_label='Temperature (Celsius)',
+                  title="Sea Surface Temperature at 43.18, -70.43")
+    plot.line('time', 'temperature', source=source)
+
+    def callback(attr, old, new):
+        if new == 0:
+            data = df
+        else:
+            data = df.rolling(f"{new}D").mean()
+        source.data = ColumnDataSource.from_df(data)
+
+    slider = Slider(start=0, end=30, value=0, step=1, title="Smoothing by N Days")
+    slider.on_change('value', callback)
+
+    doc.add_root(column(slider, plot))
+
+    # doc.theme = Theme(filename="theme.yaml")
+
---
+++ docs/projects/plotserver/bkapp/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+BOKEH_ALLOW_WS_ORIGIN=server.local:5006 python -m plotserver
